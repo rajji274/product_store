5070473899392f973481d18431f361befb12f02a
rajji274 <rajji274@gmail.com>
2025-04-18 15:48:26 +0530

    first commit 1

--- src/components/Eletricals.jsx
+++ src/components/Eletricals.jsx
@@ -17,20 +17,64 @@ function Eletricals() {
                     fetch("https://dummyjson.com/products/category/mobile-accessories"),
                     fetch("https://dummyjson.com/products/category/laptops"),
                 ]);
-                const smartPhonesData=smartPhonesRes.json(),
+                const smartPhonesData=await smartPhonesRes.json();
+                const mobileAccessriesData=await mobileAccessoriesRes.json();
+                const laptopData=await laptopsRes.json();
+
+                const combineData=[...smartPhonesData.products, ...mobileAccessriesData.products, ...laptopData.products]
+                 setProducts(combineData)
+            
+            }catch(error){
+                console.error("Error fetching here:", error)
+            }finally{
+                setLoading(false)
             }
 
-        }
-
-    })
-
+        };
+        fetchProducts();
+    },[]);
+    const filteredProducts = products.filter(product => {
+        if (userInput === "") return true;
+        return product.title.toLowerCase().includes(userInput.toLowerCase());
+    });
   return (
     <div>
-
-
-
-    </div>
-  )
+            <div className="input-card">
+                <input
+                    type="search"
+                    placeholder="Search products..."
+                    value={userInput}
+                    onChange={(e) => setUserInput(e.target.value)}
+                    style={{ width: "600px", height: "60px" }}
+                />
+            </div>
+            {
+                loading ? (
+                    <div className="loading-page">
+                        <h2 className='page-load'>Loading...</h2>
+                    </div>
+                ) : filteredProducts.length > 0 ? (
+                    <div className="beauty-wrapper">
+                       { filteredProducts.map(product => (
+                        <div className="container-beauty" key={product.id}>
+                           <img src={product.thumbnail} alt={product.title} />
+                            <p className="title">{product.title}</p>
+                            {/* <p className="description">{product.description}</p> */}
+                            <span className="rating">Rating {product.rating}</span>
+                            <p className="price">${product.price} </p>
+                            <button className="add-to-cart" onClick={()=>addToCart(product)}>Add to cart</button>
+                            
+                        </div>
+                    ))}
+                    </div>
+                ) : (
+                    <div className="no-product">
+                        <h3 className="no-data">No Products Found</h3>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
 
 export default Eletricals
